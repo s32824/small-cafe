5e44484ad307acbc418d52fd1fb308f7941136a3
Veronika Kondratova <s32824@pjwstk.edu.pl>
2026-03-24 09:25:01 +0100

    Task 2: Add CafeCustomer class with business-logic equality

--- src/CafeCustomer.java
+++ src/CafeCustomer.java
@@ -1,20 +1,27 @@
 import java.util.Objects;
+
 public class CafeCustomer {
     private int customerId;
     private String firstName;
     private String lastName;
     private String email;
+
     public CafeCustomer(int customerId, String firstName, String lastName, String email) {
         this.customerId = customerId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
-    public String getFullName() { return firstName + " " + lastName; }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
     @Override
     public String toString() {
         return "Customer: " + getFullName() + " | ID: " + customerId + " | Email: " + email;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
